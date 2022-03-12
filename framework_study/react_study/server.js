cb12a08df6fe96efd97fed2cd7dbcce2b73470e8
Godzilla <ereh_@naver.com>
2022-03-12 22:24:13 +0900

    react_setup

--- framework_study/react_study/server.js
+++ framework_study/react_study/server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 
 const app = express()
+app.use(express.static('client_side/public'))
+const axios = require('axios')
+
 
 app.get('/',(req,res)=>{
-    res.send('hi')
+
+    res.send(`<script> location.href='http://localhost:3000' </script>`)
+    
+})
+
+app.listen(5000,(req,res)=>{
+    console.log('listening_5000')
 })
