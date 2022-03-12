748e1e1a708ca1d06ef37452db440db72ad07bef
Godzilla <ereh_@naver.com>
2022-03-13 01:48:21 +0900

    memo: trying_ejs_in_react

--- framework_study/react_study/server.js
+++ framework_study/react_study/server.js
@@ -2,7 +2,10 @@ const express = require('express')
 
 const app = express()
 const path = require('path')
-// app.use(express.static(__dirname + '/client_side/build'))
+
+const Join = path.join(__dirname,'/client_side/build/' )
+app.use(express.static(Join))
+
 
 // app.set('view engine', path.join(__dirname, '/views'))
 // app.set('view engine', 'ejs');
@@ -14,8 +17,8 @@ const path = require('path')
 app.get('/',(req,res)=>{
 
     // res.send(`<script> location.href= 'http://localhost:3000' </script>`)
-    res.render(__dirname+'/client_side/build/index.ejs')
-    // res.sendFile(__dirname+'/index.html')
+    // res.sendFile(__dirname+'/client_side/build/index.html')
+    res.sendFile('/index.html')
 
 })
 
