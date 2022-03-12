0942607becdfd005a7b8d8db18aa44b581352088
Godzilla <ereh_@naver.com>
2022-03-13 00:23:39 +0900

    react_setup

--- framework_study/react_study/server.js
+++ framework_study/react_study/server.js
@@ -2,12 +2,15 @@ const express = require('express')
 
 const app = express()
 // app.use(express.static('client_side/public'))
+app.set('view engine','ejs')
 const axios = require('axios')
+const ejs = require('ejs')
 
 
 app.get('/',(req,res)=>{
 
-    res.send(`<script> location.href= 'http://localhost:3000' </script>`)
+    // res.send(`<script> location.href= 'http://localhost:3000' </script>`)
+
 
 })
 
