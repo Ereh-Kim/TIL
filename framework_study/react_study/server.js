74754a5652377d88d7829d38e69e03e0431da22a
Godzilla <ereh_@naver.com>
2022-03-12 22:39:40 +0900

    run_two_server

--- framework_study/react_study/server.js
+++ framework_study/react_study/server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 
 const app = express()
-app.use(express.static('client_side/public'))
+// app.use(express.static('client_side/public'))
 const axios = require('axios')
 
 
 app.get('/',(req,res)=>{
 
-    res.send(`<script> location.href='http://localhost:3000' </script>`)
-    
+    res.send(`<script> location.href= 'http://localhost:3000' </script>`)
+
 })
 
 app.listen(5000,(req,res)=>{
