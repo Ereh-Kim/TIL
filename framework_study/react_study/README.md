3784513bdb6d405716a54d69a2465cbcc249af24
Godzilla <ereh_@naver.com>
2022-03-14 00:29:51 +0900

    memo: TIL_add_1.React_setup_(For_develop)

--- framework_study/react_study/README.md
+++ framework_study/react_study/README.md
@@ -2,3 +2,6 @@
 
 ## 0. [React_setup][link]
 [link]:https://github.com/Ereh-Kim/TIL/blob/TIL/framework_study/react_study/React_TIL.files/0.%20React_setup.md "React_setup_링크"
+
+## 1. [React_setup_(For_develop)][link]
+[link]:"React_setup_(개발시)"
--- framework_study/react_study/server.js
+++ framework_study/react_study/server.js
@@ -1,24 +1,12 @@
 const express = require('express')
 
 const app = express()
-const path = require('path')
-
-const Join = path.join(__dirname,'/client_side/build/' )
-app.use(express.static(Join))
-
-
-// app.set('view engine', path.join(__dirname, '/views'))
-// app.set('view engine', 'ejs');
-
-// const axios = require('axios')
-// const ejs = require('ejs')
 
 
 app.get('/',(req,res)=>{
 
-    // res.send(`<script> location.href= 'http://localhost:3000' </script>`)
-    // res.sendFile(__dirname+'/client_side/build/index.html')
-    res.sendFile('/index.html')
+    res.send(`<script> location.href= 'http://localhost:3000' </script>`)
+    console.log('User_get_access_3000')
 
 })
 
