0b9bfebde57ea74c9e574f9e8f18ea93c82a6452
erehkim <ereh_@naver.com>
2022-07-07 11:23:45 +0900

    studying_stateless_componenet

---
+++ framework_study/react_study/client_side/src/components/stateless_componenet.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+class Stateless_Componenet extends Component {
+
+    render (){
+        return (
+            <React.Fragment></React.Fragment>
+        )
+    }
+
+}
