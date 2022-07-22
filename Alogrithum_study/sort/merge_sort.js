572e49b19620ae46dfa60f869c30fcd89675a037
Godzilla <ereh_@naver.com>
2022-07-22 19:22:51 +0900

    studying_Algorithum

--- Alogrithum_study/sort/merge_sort.js
+++ Alogrithum_study/sort/merge_sort.js
@@ -12,15 +12,18 @@ function Merge(left,right){
 
         if(left[0]<=right[0]){
             result.push(left.shift())
+            
         }
         else{
             result.push(right.shift())
         }
     }
-    
 
     while(left.length){ result.push(left.shift()) }
+    
+
     while(right.length){ result.push(right.shift()) }
+    
 
     return result
 
