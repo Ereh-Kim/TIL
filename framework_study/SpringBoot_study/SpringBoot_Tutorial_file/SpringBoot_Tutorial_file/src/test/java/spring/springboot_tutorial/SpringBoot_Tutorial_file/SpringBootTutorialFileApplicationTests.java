d88c97d30539e282e5ce946a00bc8f997b589f84
Godzilla <ereh_@naver.com>
2022-12-31 17:11:54 +0900

    :seedling: SpringBoot_tutorial_file_setup

---
+++ framework_study/SpringBoot_study/SpringBoot_Tutorial_file/SpringBoot_Tutorial_file/src/test/java/spring/springboot_tutorial/SpringBoot_Tutorial_file/SpringBootTutorialFileApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springboot_tutorial.SpringBoot_Tutorial_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
