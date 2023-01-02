bdb535903b4c4ca7a8be50ddce6fad1877098bd7
Godzilla <ereh_@naver.com>
2023-01-02 16:19:26 +0900

    :tada: following_springboot_tutorial_Setup&RunningServer_webVersion

--- framework_study/SpringBoot_study/SpringBoot_Tutorial_file/SpringBoot_Tutorial_file/src/main/java/spring/springboot_tutorial/SpringBoot_Tutorial_file/SpringBootTutorialFileApplication.java
+++ framework_study/SpringBoot_study/SpringBoot_Tutorial_file/SpringBoot_Tutorial_file/src/main/java/spring/springboot_tutorial/SpringBoot_Tutorial_file/SpringBootTutorialFileApplication.java
@@ -2,12 +2,21 @@ package spring.springboot_tutorial.SpringBoot_Tutorial_file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringBootTutorialFileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTutorialFileApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public List<String> SayHello() {
+		return List.of("hello","internet");
 	}
 
 }
